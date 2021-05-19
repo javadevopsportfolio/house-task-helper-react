a75b0a499ad60dbcffb7158bc8be2e92829a09b3
blayde26 <brianmlayden@gmail.com>
2021-05-19 17:43:36 -0400

    Adding Shell project
    Adding Skeleton with Login and Signup Pages

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Task Tracker</h1>
+        <p className="text-muted">A task helper for around the house.</p>
+      </div>
+    </div>
+  );
+}
